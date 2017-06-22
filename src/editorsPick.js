94c83c825b1c3aa063068e37f0c1d10e0e063dba
Alex <alex.satur89@gmail.com>
2017-06-22 17:09:32 +0100

    Editors picks showing on index.html. By pulling the info from the api and extracting the news headlines from there :-)

--- src/editorsPick.js
+++ src/editorsPick.js
@@ -1,8 +1,16 @@
 function loadHeadlines(data){
+  var newData = JSON.parse(data)
   var articles = '<ul>';
-  data['response']['editorsPicks'].forEach(function(article){
+  newData['response']['editorsPicks'].forEach(function(article){
     articles += ("<li><div>" + article['webTitle'] + "</div></li>")
   });
   articles += '</ul>'
   return articles
+}
+
+function retrieveHeadlines(){
+  var request = new XMLHttpRequest();
+  request.open('GET', 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/uk?show-editors-picks=true', false)
+  request.send();
+  return request.response
 }
